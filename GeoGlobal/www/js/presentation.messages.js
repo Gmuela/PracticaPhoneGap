840bef8e5b9eed692ae5e81d5d4fa4c6b499d9a8
Gmuela <gmuela92@gmail.com>
2017-02-23 14:15:45 +0100

    Terminado la parte de messages

--- GeoGlobal/www/js/presentation.messages.js
+++ GeoGlobal/www/js/presentation.messages.js
@@ -2,8 +2,15 @@ function setTexts() {
     $("#titulo_app").html(mgs_titulo_app);
     $("#boton_geolocal").html(msg_boton_iniciar_app);
 
-    $("#titulo_geolocal").html(msg_boton_iniciar_app);
-    $("#boton_geolocal").html(msg_boton_iniciar_app);
-    $("#boton_geolocal").html(msg_boton_iniciar_app);
+    $("#titulo_geolocal").html(msg_titulo_geolocal);
+    $("#contenido_geolocal").html(msg_explicacion_geolocal);
+    $("#boton_siguiente_geolocal").html(msg_btn_next);
 
+    $("#titulo_lenguaje").html(msg_titulo_language);
+    $("#contenido_lenguaje").html(msg_explicacion_language);
+    $("#boton_siguiente_lenguaje").html(msg_btn_next);
+
+    $("#titulo_idieciochon").html(msg_titulo_i18n);
+    $("#contenido_idieciochon").html(msg_explicacion_i18n);
+    $("#boton_finalizar").html(msg_btn_back);
 }
