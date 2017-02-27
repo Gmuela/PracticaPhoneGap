e7f2b2d52ba30d6cdd2a7a10c7634e989ccf3a35
Gmuela <gmuela92@gmail.com>
2017-02-27 10:24:57 +0100

    Pruebas añadidas

--- GeoGlobal/www/js/apisScripts.js
+++ GeoGlobal/www/js/apisScripts.js
@@ -37,12 +37,13 @@ function setLanguage() {
         if (navigator && navigator.userAgent && (lang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) {
             lang = lang[1];
         }
-
+        alert(navigator.userAgent);
+        alert(navigator.language);
+        alert(navigator.systemLanguage);
+        alert(navigator.userLanguage);
         if (!lang && navigator) {
             if (navigator.language) {
                 lang = navigator.language;
-            } else if (navigator.browserLanguage) {
-                lang = navigator.browserLanguage;
             } else if (navigator.systemLanguage) {
                 lang = navigator.systemLanguage;
             } else if (navigator.userLanguage) {
